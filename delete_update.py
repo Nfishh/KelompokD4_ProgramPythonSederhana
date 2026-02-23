504425192c62e69bafadebf96fec8c9f5ac0080b
zulfi-real <julfiacc4disc@gmail.com>
2026-02-23 22:49:22 +0700

    feat(edit): add delete and update feature

---
+++ delete_update.py
@@ -0,0 +1,16 @@
+
+def menu_edit(data):
+    id_target = int(input("Masukkan ID tugas: "))
+    for t in data:
+        if t['id'] == id_target:
+            print("1. Hapus | 2. Tandai Selesai")
+            opsi = input("Pilih: ")
+            if opsi == "1":
+                data.remove(t)
+                print("🗑️ Berhasil dihapus")
+            elif opsi == "2":
+                t['status'] = "Selesai"
+                print("✔️ Status diperbarui")
+            return data
+    print("⚠️ ID tidak ditemukan")
+    return data
